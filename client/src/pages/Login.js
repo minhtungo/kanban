71360c34d7cc795e74db052b6a7632e7692a0c66
minhtungo <loducfc@gmail.com>
2022-07-20 01:54:26 -0400

    Set Up Login Page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,50 @@
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Box, Button, TextField } from '@mui/material';
+import LoadingButton from '@mui/lab/LoadingButton';
+
 const Login = () => {
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async () => {};
+
   return (
-    <div>Login</div>
-  )
-}
-export default Login
+    <>
+      <Box component='form' sx={{ mt: 1 }} onSubmit={handleSubmit} noValidate>
+        <TextField
+          margin='normal'
+          required
+          fullWidth
+          id='userName'
+          label='Username'
+          name='userName'
+          disabled={loading}
+        />
+        <TextField
+          margin='normal'
+          required
+          fullWidth
+          id='password'
+          label='Password'
+          name='password'
+          type='password'
+          disabled={loading}
+        />
+        <LoadingButton
+          sx={{ mt: 3, mb: 2 }}
+          variant='outlined'
+          fullWidth
+          color='success'
+          type='submit'
+          loading={loading}
+        >
+          Login
+        </LoadingButton>
+      </Box>
+      <Button component={Link} to='/signup' sx={{ textTransform: 'none' }}>
+        Do not have an account? Signup now
+      </Button>
+    </>
+  );
+};
+export default Login;
