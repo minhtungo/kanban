0b072c6fe5bd1a233d1330553cb362906a3692c1
minhtungo <loducfc@gmail.com>
2022-07-28 03:27:19 -0400

    Set up board component

--- client/src/pages/Board.js
+++ client/src/pages/Board.js
@@ -1,14 +1,25 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import boardApi from '../api/boardApi';
-import { Box } from '@mui/material';
+import {
+  Box,
+  Button,
+  Divider,
+  IconButton,
+  TextField,
+  Typography,
+} from '@mui/material';
+import StarOutlinedIcon from '@mui/icons-material/StarOutlined';
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
+import { EmojiPicker } from 'emoji-mart';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 
 const Board = () => {
   const { boardId } = useParams();
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [sections, setSections] = useState([]);
-  const [isFavorite, setIsFavorite] = useState('');
+  const [isStarred, setIsStarred] = useState('');
   const [icon, setIcon] = useState('');
 
   useEffect(() => {
@@ -18,7 +29,7 @@ const Board = () => {
         setTitle(res.title);
         setDescription(res.description);
         setSections(res.sections);
-        setIsFavorite(res.favorite);
+        setIsStarred(res.starred);
         setIcon(res.icon);
         console.log(res);
       } catch (error) {
@@ -39,8 +50,60 @@ const Board = () => {
         }}
       >
         <IconButton variant='outlined'>
-
+          {isStarred ? (
+            <StarOutlinedIcon color='warning' />
+          ) : (
+            <StarBorderOutlinedIcon />
+          )}
         </IconButton>
+        <IconButton variant='outlined' color='error'>
+          <DeleteOutlinedIcon />
+        </IconButton>
+      </Box>
+      <Box sx={{ padding: '10px 50px' }}>
+        <Box>
+          <TextField
+            value={title}
+            placeholder='Untitled'
+            variant='outlined'
+            fullWidth
+            sx={{
+              '& .MuiOutlinedInput-input': { padding: 0 },
+              '& .MuiOutlinedInput-notchedOutline': { border: 'unset' },
+              '& .MuiOutlinedInput-root': {
+                fontSize: '2rem',
+                fontWeight: '700',
+              },
+            }}
+          />
+          <TextField
+            value={description}
+            placeholder='Add a description here'
+            variant='outlined'
+            fullWidth
+            sx={{
+              '& .MuiOutlinedInput-input': { padding: 0 },
+              '& .MuiOutlinedInput-notchedOutline': { border: 'unset' },
+              '& .MuiOutlinedInput-root': { fontSize: '0.8rem' },
+            }}
+          />
+        </Box>
+
+        <Box>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Button>Add section</Button>
+            <Typography variant='body2' fontWeight='700'>
+              {sections.length} Sections
+            </Typography>
+          </Box>
+          <Divider sx={{ margin: '10px 0' }} />
+        </Box>
       </Box>
     </>
   );
