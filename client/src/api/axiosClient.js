a4b1b36ce0721cf4fc0b3daa2f6afe91c213b42b
minhtungo <loducfc@gmail.com>
2022-08-01 14:31:21 -0400

    change baseURL for connecting backend to deploy

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import queryString from 'query-string';
 
-const baseUrl = 'api/v1/';
+const baseUrl = 'https://minh-kanban.herokuapp.com/api/v1/';
 const getToken = () => localStorage.getItem('token');
 
 const axiosClient = axios.create({
