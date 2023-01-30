351004613c978b4a81ae7cd892086ee28a9db2af
Minh Tu Ngo <loducfc@gmail.com>
2023-01-30 03:29:57 -0500

    Update baseurl

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import queryString from 'query-string';
 
-const baseUrl = 'https://minh-kanban.herokuapp.com/api/v1/';
+const baseUrl = 'https://kanban-u56m.onrender.com/api/v1/';
 const getToken = () => localStorage.getItem('token');
 
 const axiosClient = axios.create({
--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, './client/build')));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
